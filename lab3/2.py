998afa56e59d6481d904533e449d46c73abcf745
konstaolimpov <bobylev.aiu@phystech.edu>
2019-09-20 19:35:26 +0300

    1.py

---
+++ lab3/2.py
@@ -0,0 +1,22 @@
+from graph import*
+
+brushColor('yellow')
+penColor('white')
+circle(400,60,50)
+
+brushColor('white')
+penColor('black')
+circle(100,50,20)
+circle(130,50,20)
+circle(80,65,20)
+circle(110,65,20)
+circle(145,65,20)
+circle(160,50,20)
+circle(170,65,20)
+
+brushColor(0,0,50)
+penSize(300)
+penColor(0,0,50)
+line(0,500,1000,500)
+
+run()
